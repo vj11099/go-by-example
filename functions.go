746eeb231b81c6f256ae1ac05592dbe6e2bdfeef
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2024-11-23 17:19:44 +0530

    Go by examples functions that return multiple values

--- functions.go
+++ functions.go
@@ -6,8 +6,17 @@ func add(a, b, c int) int {
 	return a + b + c
 }
 
-func main() {
-	res := add(10, 13, 49)
+func vals() (int, int, int) {
+	return 10, 13, 49
+}
 
+func main() {
+
+	a, b, c := vals()
+	fmt.Println("a from vals:", a)
+	fmt.Println("b from vals:", b)
+	fmt.Println("c from vals:", c)
+
+	res := add(10, 13, 49)
 	fmt.Println("sum:", res)
 }
