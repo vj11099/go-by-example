c0c007ae1d128a5471d68e381d3f871e2349006b
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2024-12-01 18:16:56 +0530

    Go by examples- enums and restructuring folders

---
+++ enums/enums.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
+func main() {
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+
+	ns2 := transition(ns)
+	fmt.Println(ns2)
+
+	//error state
+	ns3 := transition(3)
+	fmt.Println(ns3)
+
+	//throws an error (out of bounds / unknown state)
+	ns4 := transition(6)
+	fmt.Println(ns4)
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
+}
