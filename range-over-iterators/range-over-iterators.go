2310ad5c4615f370fa2d599c4df54c4a215fdb55
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2024-12-04 11:44:08 +0530

    Go by examples- range over iterators (pending)

---
+++ range-over-iterators/range-over-iterators.go
@@ -0,0 +1,102 @@
+package main
+
+// type listItem[T any] struct {
+// 	next *listItem[T]
+// 	val  T
+// }
+
+// type List[T any] struct {
+// 	head, tail *listItem[T]
+// }
+
+// func (lst *List[T]) Push(v T) {
+// 	if lst.tail == nil {
+// 		lst.tail = &listItem[T]{val: v}
+// 		lst.head = lst.tail
+// 	} else {
+// 		lst.tail.next = &listItem[T]{val: v}
+// 		lst.tail = lst.tail.next
+// 	}
+// }
+
+// func main() {
+// 	lst := List[int]{}
+// 	lst.Push(13)
+// 	lst.Push(71)
+// 	lst.Push(22)
+// 	lst.Push(97)
+
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"iter"
+// 	"slices"
+// )
+
+// type List[T any] struct {
+// 	head, tail *element[T]
+// }
+
+// type element[T any] struct {
+// 	next *element[T]
+// 	val  T
+// }
+
+// func (lst *List[T]) Push(v T) {
+// 	if lst.tail == nil {
+// 		lst.head = &element[T]{val: v}
+// 		lst.tail = lst.head
+// 	} else {
+// 		lst.tail.next = &element[T]{val: v}
+// 		lst.tail = lst.tail.next
+// 	}
+// }
+
+// func (lst *List[T]) All() iter.Seq[T] {
+// 	return func(yield func(T) bool) {
+
+// 		for e := lst.head; e != nil; e = e.next {
+// 			if !yield(e.val) {
+// 				return
+// 			}
+// 		}
+// 	}
+// }
+
+// func genFib() iter.Seq[int] {
+// 	return func(yield func(int) bool) {
+// 		a, b := 1, 1
+
+// 		for {
+// 			if !yield(a) {
+// 				return
+// 			}
+// 			a, b = b, a+b
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	lst := List[int]{}
+// 	lst.Push(10)
+// 	lst.Push(13)
+// 	lst.Push(23)
+
+// 	for e := range lst.All() {
+// 		fmt.Println(e)
+// 	}
+
+// 	all := slices.Collect(lst.All())
+// 	fmt.Println("all:", all)
+
+// 	for n := range genFib() {
+
+// 		if n >= 10 {
+// 			break
+// 		}
+// 		fmt.Println(n)
+// 	}
+// }
