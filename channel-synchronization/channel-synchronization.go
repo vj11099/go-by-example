4e5a99894a0885dd13080ebe1789efb300e52156
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2024-12-05 15:11:05 +0530

    Go by examples- go channel synchronizatino

---
+++ channel-synchronization/channel-synchronization.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Printf("Working...")
+	time.Sleep(time.Second)
+	fmt.Println("Finished!")
+
+	done <- true
+}
+
+func main() {
+
+	done := make(chan bool)
+	go worker(done)
+	<-done //waits for the channel task to complete
+	go worker(done)
+	<-done //waits for the channel task to complete
+	fmt.Println("All tasks are completed succesfully")
+}
